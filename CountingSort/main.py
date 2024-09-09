593fb12dc5da777102070fa1c8e5d3be14cc7aca
Daniel Solano <solanomezaangel@gmail.com>
2024-09-09 00:29:22 -0700

    Ok

--- CountingSort/main.py
+++ CountingSort/main.py
@@ -1,7 +1,6 @@
 from manim import *
 import random
 
-
 def generador_entrada(size):
     vector = []
     for i in range(size):
@@ -9,29 +8,25 @@ def generador_entrada(size):
         vector.append(num)
     return vector
 
-
 class CountingSort(Scene):
     def construct(self):
-        # Definir los elementos del arreglo
-
-        array = generador_entrada(10)
+        self.camera.frame_rate = 60
+        array = generador_entrada(11)
 
         boxes = []
         groups = []
         for i, value in enumerate(array):
             box = Square(side_length=1, color=RED).move_to([-5 + (i), 0, 0])
             boxes.append(box)
-
-            # Crear el texto con el valor del elemento dentro del rectángulo
             index = Text(str(i), font_size=24).move_to(box.get_center() + UP)
             text = Text(str(value)).move_to(box.get_center())
             groups.append(VGroup(box, text, index))
 
         array_group = VGroup(*groups)
         array_group.arrange()
-        self.play(Create(array_group))
-        self.wait(2)
-        self.play(array_group.animate.shift(UP * 2))
+        self.play(Create(array_group), run_time=1)  # Ajustar velocidad
+        self.wait(1)
+        self.play(array_group.animate.shift(UP * 2), run_time=1)
 
         max_value = max(array)
         counter = [0] * (max_value + 1)
@@ -40,108 +35,90 @@ class CountingSort(Scene):
         c_groups = []
 
         for i, value in enumerate(counter):
-            c_box = Square(side_length=1, color=LIGHT_PINK).move_to(
-                [-5 + (i), 0, 0])
+            c_box = Square(side_length=1, color=LIGHT_PINK).move_to([-5 + (i), 0, 0])
             c_boxes.append(c_box)
-
             c_text = Text(str(value)).move_to(c_box.get_center())
-            c_index = Text(str(i), font_size=24).move_to(
-                c_box.get_center() + UP)
+            c_index = Text(str(i), font_size=24).move_to(c_box.get_center() + UP)
             c_groups.append(VGroup(c_box, c_text, c_index))
 
         c_array_group = VGroup(*c_groups)
         c_array_group.arrange()
-        self.play(Create(c_array_group))
+        self.play(Create(c_array_group), run_time=1)
         self.wait(1)
-        self.play(c_array_group.animate.shift(DOWN * 1))
+        self.play(c_array_group.animate.shift(DOWN * 1), run_time=1)
 
-        j = 0
-        for i in array:
+        # Primer ciclo
+        for j, i in enumerate(array):
             s2 = SurroundingRectangle(groups[j][0], color=YELLOW)
-            self.play(Write(s2))
-            counter[i] = counter[i] + 1
+            self.play(Write(s2), run_time=0.5)
+            counter[i] += 1
             s = SurroundingRectangle(c_groups[i], color=YELLOW)
-            self.play(Write(s))
-            self.play(Transform(c_groups[i][1], Text(
-                str(counter[i])).move_to(c_boxes[i].get_center())))
+            self.play(Write(s), run_time=0.5)
+            self.play(Transform(c_groups[i][1], Text(str(counter[i])).move_to(c_boxes[i].get_center())), run_time=0.5)
             color_change = boxes[j].copy().set_color(BLUE)
-            self.play(Transform(boxes[j], color_change))
-            c_boxes[i].set_color(LIGHT_PINK)
-            self.play(Uncreate(s))
-            self.play(Uncreate(s2))
-            
-            j += 1
-        
+            self.play(Transform(boxes[j], color_change), run_time=0.5)
+            self.play(Uncreate(s), run_time=0.5)
+            self.play(Uncreate(s2), run_time=0.5)
+
         array_copy = array_group.copy()
-        self.play(FadeOut(array_group))
-        self.play(c_array_group.animate.shift(UP * 1))
-        
+        self.play(FadeOut(array_group), run_time=1)
+        self.play(c_array_group.animate.shift(UP * 1), run_time=1)
+
+        # Segundo ciclo
         for i in range(1, len(counter)):
-            add_g = VGroup(c_boxes[i-1], c_boxes[i] )
+            add_g = VGroup(c_boxes[i - 1], c_boxes[i])
             sa = SurroundingRectangle(add_g, color=YELLOW)
-            self.play(Write(sa))    
+            self.play(Write(sa), run_time=0.5)
             counter[i] += counter[i - 1]
             add_t = Text(str(counter[i])).move_to(c_boxes[i].get_center())
-            self.play(Transform(c_groups[i][1], add_t))
-            self.play(FadeOut(sa))
-            
-
+            self.play(Transform(c_groups[i][1], add_t), run_time=0.5)
+            self.play(FadeOut(sa), run_time=0.5)
 
         sorted_array = [0] * 10
         s_boxes = []
         s_groups = []
         for i, value in enumerate(sorted_array):
-            s_box = Square(side_length=1, color=GREEN).move_to(
-                [-5 + (i), 0, 0])
+            s_box = Square(side_length=1, color=GREEN).move_to([-5 + (i), 0, 0])
             s_boxes.append(s_box)
             s_text = Text(str(value)).move_to(s_box.get_center())
-            s_index = Text(str(i), font_size=24).move_to(
-                s_box.get_center() + UP)
+            s_index = Text(str(i), font_size=24).move_to(s_box.get_center() + UP)
             s_groups.append(VGroup(s_box, s_text, s_index))
         s_array_group = VGroup(*s_groups)
         s_array_group.arrange()
-        
-        
-            
-        self.play(Create(array_copy))
-        self.play(array_copy.animate.shift(UP * 1)) 
-        self.play(c_array_group.animate.shift(DOWN * 2))
-        self.play(Create(s_array_group))
-        self.play(array_copy.animate.shift(DOWN * 1)) 
-        
+
+        self.play(Create(array_copy), run_time=1)
+        self.play(array_copy.animate.shift(UP * 1), run_time=1)
+        self.play(c_array_group.animate.shift(DOWN * 2), run_time=1)
+        self.play(Create(s_array_group), run_time=1)
+        self.play(array_copy.animate.shift(DOWN * 1), run_time=1)
+
         for i in range(len(array) - 1, -1, -1):
-            s_s = SurroundingRectangle(array_copy[i][1], color=YELLOW)  # Resalta el elemento del array original
-            s_c = SurroundingRectangle(c_groups[array[i]][2], color=YELLOW)  # Resalta el índice en el contador
+            s_s = SurroundingRectangle(array_copy[i][1], color=YELLOW)  
+            s_c = SurroundingRectangle(c_groups[array[i]][2], color=YELLOW)  
+            self.play(Write(s_s), run_time=0.5)
+            self.play(Write(s_c), run_time=0.5)
 
-            self.play(Write(s_s))
-            self.play(Write(s_c))
-            
             pos = counter[array[i]] - 1
-
             counter[array[i]] -= 1
-            
             minus = Text(str(counter[array[i]])).move_to(c_boxes[array[i]].get_center())
-            self.play(Transform(c_groups[array[i]][1], minus))
-            
-            highlight_index = SurroundingRectangle(s_array_group[pos][2], color=GREEN)
-            self.play(Create(highlight_index))
+            self.play(Transform(c_groups[array[i]][1], minus), run_time=0.5)
 
-            highlight_box = SurroundingRectangle(s_array_group[pos][0], color=YELLOW)
-            self.play(Create(highlight_box))
+            highlight_index = SurroundingRectangle(s_array_group[pos][2], color=GREEN)
+            self.play(Create(highlight_index), run_time=0.5)
+
+            highlight_box = SurroundingRectangle(s_array_group[pos][0], color=GREEN)
+            self.play(Create(highlight_box), run_time=0.5)
 
             number = Text(str(array[i])).move_to(s_boxes[pos].get_center())
-            self.play(Transform(s_array_group[pos][1], number))
+            self.play(Transform(s_array_group[pos][1], number), run_time=0.5)
 
             green_box = s_boxes[pos].copy().set_color(GREEN)
-            self.play(Transform(s_boxes[pos], green_box))
-            
-            
+            self.play(Transform(s_boxes[pos], green_box), run_time=0.5)
+
             red = boxes[i].copy().set_color(RED)
-            self.play(Transform(boxes[i], red))
+            self.play(Transform(boxes[i], red), run_time=0.5)
 
-            # Desaparecer los rectángulos de resaltado
-            self.play(FadeOut(s_s))
-            self.play(FadeOut(s_c))
-            self.play(FadeOut(highlight_index))
-            self.play(FadeOut(highlight_box))
-
+            self.play(FadeOut(s_s), run_time=0.5)
+            self.play(FadeOut(s_c), run_time=0.5)
+            self.play(FadeOut(highlight_index), run_time=0.5)
+            self.play(FadeOut(highlight_box), run_time=0.5)
